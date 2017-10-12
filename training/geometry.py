8cfdf5a2d6306bd4cda7342a0b3325cd6f030960
ja-vpaw <vpaw6@ya.ru>
2017-10-12 12:53:20 +0300

    t13 - compare list

--- training/geometry.py
+++ training/geometry.py
@@ -2,8 +2,10 @@ __author__ = 'ja'
 
 from geom2d import *
 
-a = Point(0, 0)
-b = Point(3, 4)
-print(a.distance(b))
-print(a == b)
-print(a == Point(0, 0))
+
+l1 = [Point(0,0), Point(1,2), Point(2,1)]
+# l2 = [Point(0,0), Point(1,2), Point(2,1)]
+l2 = list(l1)
+l2[0] = Point(0,0)
+
+print(l1 == l2)
