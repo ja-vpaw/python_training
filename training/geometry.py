5590524aa133efc6739d9735255d16dd8eeb2d01
ja-vpaw <vpaw6@ya.ru>
2017-10-12 18:13:55 +0300

    t18 - map_filter

--- training/geometry.py
+++ training/geometry.py
@@ -3,9 +3,9 @@ __author__ = 'ja'
 from geom2d import *
 
 
-l = [Point(i, i*i) for i in range(-5, 6)]
+l = list(map(lambda i: Point(i, i*i), range(-5, 6)))
 
-l2 = [Point(el.x, -el.y) for el in l]
+l2 = list(filter(lambda p: p.x % 2 == 0, l))
 
 print(l)
 print(l2)
