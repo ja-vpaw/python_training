4b6f31b67be73ed010d02acf8d7eb96712b10324
ja-vpaw <vpaw6@ya.ru>
2017-10-12 14:40:24 +0300

    t16 - lampda

--- training/geometry.py
+++ training/geometry.py
@@ -3,8 +3,9 @@ __author__ = 'ja'
 from geom2d import *
 
 
-l1 = [Point(0,0), Point(1,2), Point(2,1)]
-l2 = sorted(l1)
+l1 = [Point(3,1), Point(0,0), Point(1,2)]
+
+l2 = sorted(l1, key=lambda p: p.x)
 
 print(l1)
 print(l2)
