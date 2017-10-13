785d2b619b4be351d5b82f208e8252ea6363b435
ja-vpaw <vpaw6@ya.ru>
2017-10-13 15:28:45 +0300

    4-5 add randomization in del and modify tests

--- fixture/group.py
+++ fixture/group.py
@@ -44,10 +44,18 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
 
+    def select_group_by_index(self, index):
+        wd = self.app.wd
+        wd.find_elements_by_name("selected[]")[index].click()
+
     def delete_first_group(self):
         wd = self.app.wd
+        self.delete_group_by_index(0)
+
+    def delete_group_by_index(self, index):
+        wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
@@ -55,8 +63,12 @@ class GroupHelper:
 
     def modify_first_group(self , new_group_data):
         wd = self.app.wd
+        self.modify_group_by_index(0, new_group_data)
+
+    def modify_group_by_index(self , index, new_group_data):
+        wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # open modification form
         wd.find_element_by_name("edit").click()
         # fill group form
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,17 +1,19 @@
 __author__ = 'ja'
 
 from model.group import Group
+from random import randrange
 
 def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="Test"))
     old_groups = app.group.get_group_list()
+    index = randrange(len(old_groups))
     group_mod = Group(name="New group")
-    group_mod.id = old_groups[0].id
-    app.group.modify_first_group(group_mod)
+    group_mod.id = old_groups[index].id
+    app.group.modify_group_by_index(index, group_mod)
     assert len(old_groups) == app.group.count()
     new_groups = app.group.get_group_list()
-    old_groups[0] = group_mod
+    old_groups[index] = group_mod
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
