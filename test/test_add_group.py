7f9e1626f29aad670df0ac323654862c9d241450
ja-vpaw <vpaw6@ya.ru>
2017-10-13 14:04:16 +0300

    4-4 add caching and hashing optimization for tests

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-from sys import maxsize
+
 
 def test_add_new_group(app):
     old_groups = app.group.get_group_list()
     group_add = Group("dsfdsf" , header="sdfdsf" , footer="sdfsdfds")
     app.group.create(group_add)
+    assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
     old_groups.append(group_add)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-def test_add_empty_group(app):
-    old_groups = app.group.get_group_list()
-    group_add = Group(name="" , header="" , footer="")
-    app.group.create(group_add)
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
-    old_groups.append(group_add)
-    assert sorted(old_groups , key=Group.id_or_max) == sorted(new_groups , key=Group.id_or_max)
+# def test_add_empty_group(app):
+#     old_groups = app.group.get_group_list()
+#     group_add = Group(name="" , header="" , footer="")
+#     app.group.create(group_add)
+#     new_groups = app.group.get_group_list()
+#     assert len(old_groups) + 1 == len(new_groups)
+#     old_groups.append(group_add)
+#     assert sorted(old_groups , key=Group.id_or_max) == sorted(new_groups , key=Group.id_or_max)
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -9,8 +9,8 @@ def test_modify_group_name(app):
     group_mod = Group(name="New group")
     group_mod.id = old_groups[0].id
     app.group.modify_first_group(group_mod)
+    assert len(old_groups) == app.group.count()
     new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
     old_groups[0] = group_mod
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
