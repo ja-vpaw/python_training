2b928179b0a897ca3a95566af8ee76553619bde1
ja-vpaw <vpaw6@ya.ru>
2017-10-13 11:25:45 +0300

    4-3 add assert for sorting list

--- model/group.py
+++ model/group.py
@@ -1,5 +1,6 @@
 __author__ = 'ja'
 
+from sys import maxsize
 
 class Group:
 
@@ -13,4 +14,10 @@ class Group:
         return "%s:%s" % (self.id, self.name)
 
     def __eq__(self, other):
-        return self.id == other.id and self.name == other.name
+        return (self.id is None or other.id is None or self.id == other.id) and self.name == other.name
+
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -6,14 +6,19 @@ def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="Test"))
     old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(name="New group"))
+    group_mod = Group(name="New group")
+    group_mod.id = old_groups[0].id
+    app.group.modify_first_group(group_mod)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group_mod
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-def test_modify_group_header(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name="Test" , header="Test_Header"))
-    old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(header="New header"))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
+
+# def test_modify_group_header(app):
+#     if app.group.count() == 0:
+#         app.group.create(Group(name="Test" , header="Test_Header"))
+#     old_groups = app.group.get_group_list()
+#     app.group.modify_first_group(Group(header="New header"))
+#     new_groups = app.group.get_group_list()
+#     assert len(old_groups) == len(new_groups)
