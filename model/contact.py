966454dd376d60cde39bd588a65f0a61aab43b80
ja-vpaw <vpaw6@ya.ru>
2017-10-16 12:57:40 +0300

    5-3 merging lines

--- model/contact.py
+++ model/contact.py
@@ -4,14 +4,17 @@ from sys import maxsize
 
 class Contact:
 
-    def __init__(self, firstname=None, lastname=None, homephone=None, mobilephone=None, workphone=None, secondaryphone=None, id=None):
+    def __init__(self, id=None, firstname=None, lastname=None, homephone=None,
+                 mobilephone=None, workphone=None, secondaryphone=None, all_phones_from_home_page=None):
+        self.id = id
         self.firstname = firstname
         self.lastname = lastname
         self.homephone = homephone
         self.mobilephone = mobilephone
         self.workphone = workphone
         self.secondaryphone = secondaryphone
-        self.id = id
+        self.all_phones_from_home_page = all_phones_from_home_page
+
 
     def __repr__(self):
         return "%s:%s %s" % (self.id, self.firstname, self.lastname)
