ac11e07acf883ae45a8568980558f15ba96c6270
ja-vpaw <vpaw6@ya.ru>
2017-10-17 10:03:16 +0300

    5-4 add ddt parametrize data

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,21 +1,31 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+import pytest
+import random
+import string
 
 
-def test_add_new_group(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix+"".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata_matrix = [
+    Group(name=name, header=header, footer=footer)
+    for name in ["", random_string("name_", 10)]
+    for header in ["", random_string("header_", 20)]
+    for footer in ["", random_string("footer_", 10)]
+]
+
+testdata = [Group(name="", header="", footer="")] + [
+    Group(name=random_string("name_", 10), header=random_string("header_", 20), footer=random_string("footer_", 20))
+    for i in range(5)
+]
+
+@pytest.mark.parametrize("group_add", testdata, ids=[repr (x) for x in testdata])
+def test_add_new_group(app, group_add):
     old_groups = app.group.get_group_list()
-    group_add = Group("dsfdsf" , header="sdfdsf" , footer="sdfsdfds")
     app.group.create(group_add)
     assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
     old_groups.append(group_add)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-# def test_add_empty_group(app):
-#     old_groups = app.group.get_group_list()
-#     group_add = Group(name="" , header="" , footer="")
-#     app.group.create(group_add)
-#     new_groups = app.group.get_group_list()
-#     assert len(old_groups) + 1 == len(new_groups)
-#     old_groups.append(group_add)
-#     assert sorted(old_groups , key=Group.id_or_max) == sorted(new_groups , key=Group.id_or_max)
