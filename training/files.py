a1095d0b2d5b0e5f2d6590c8a979a14ceed3cb32
ja-vpaw <vpaw6@ya.ru>
2017-10-18 09:20:40 +0300

    t27 - with

--- training/files.py
+++ training/files.py
@@ -2,13 +2,14 @@ __author__ = 'ja'
 
 import json
 
-f = open("config.json")
-try:
-    res = json.load(f)
-except ValueError as ex:
-    print(ex)
-    res = {}
-finally:
-    f.close()
+# f = open("config.json")
+with open("config.json") as f:
+    try:
+        res = json.load(f)
+    except ValueError as ex:
+        print(ex)
+        res = {}
+    # finally:
+    #     f.close()
 
 print(res)
